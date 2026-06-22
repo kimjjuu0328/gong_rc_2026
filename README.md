03274e8a958cdcb82796533fd4da481b326bf1ff
kimjjuu0328 <kimjjuu0328@gmail.com>
2026-06-22 15:18:52 +0900

    print

--- README.md
+++ README.md
@@ -5,5 +5,5 @@
 
 공주대에서 autocar 를 가지고 하는 프로젝트 수업
 첫번째 local dptj 만든 커밋 입니다.
-git config -- user.name "주형"
+git config -- user.name "kimjjuu0328"
 git config -- user.email "kimjjuu0328@gmail.com"
---
+++ python/a04_print.py
@@ -0,0 +1,13 @@
+class A_test:
+    def _repr_(self):
+    return "A test 객체이다."
+
+def main():
+    print(12345)
+    print(123,"kim","joo","hyung")
+    print(3.12345)
+    A = A_test()
+
+    print("this is","python", "class!!",sep="_",end="")
+    print("this is","python", "class!!",sep="-")
+
