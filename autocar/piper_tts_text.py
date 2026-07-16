e50013cecab27d0c368e4742046a368015b4b945
kimjjuu0328 <kimjjuu0328@gmail.com>
2026-07-16 10:47:56 +0900

    2026/07/15
    Add instructions for using Piper TTS in piper_tts_text.py

---
+++ autocar/piper_tts_text.py
@@ -0,0 +1,5 @@
+# pip install piper-tts
+# wget https://huggingface.co/rhasspy/piper-voices/resolve/main/en/en_US/lessac/medium/en_US-lessac-medium.onnx
+# wget https://huggingface.co/rhasspy/piper-voices/resolve/main/en/en_US/lessac/medium/en_US-lessac-medium.onnx.json
+# echo "Hello, this is Piper text to speech." | \
+# piper --model ~/piper_models/en_US-lessac-medium.onnx --output_file hello.wav
